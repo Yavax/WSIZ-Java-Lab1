f972769a842e928b583539037b8bc4eec110c2b7
Yavax <yavax.dev@gmail.com>
2017-10-09 13:23:25 +0200

    Add FirstWindow class

---
+++ src/WindowApp/FirstWindow.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WindowApp;
+
+/**
+ *
+ * @author Ja
+ */
+public class FirstWindow {
+    
+    public static void main(String[] args){
+        
+    }
+}
