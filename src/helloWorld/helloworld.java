71cc91ded623cad5dbaa2a03375b1768cfaec96a
Yavax <yavax.dev@gmail.com>
2017-10-09 13:19:33 +0200

    Refactor class

--- src/helloWorld/helloworld.java
+++ src/helloWorld/helloworld.java
@@ -1,3 +1,5 @@
+package helloWorld;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,13 +8,10 @@
 
 /**
  *
- * @author Ja
+ * @author Pawel
  */
 public class helloworld {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         for(int i = 0; i<args.length; i++){
